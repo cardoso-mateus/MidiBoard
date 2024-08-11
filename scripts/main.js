df14a1b145404b8da0b60ca7a15a555e25b1198d
Mateus Cardoso <cardoso.mateusv@gmail.com>
2024-08-10 21:08:58 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,60 @@
-var tecla_pom = document.querySelector('.tecla_pom');
+var teclas = [
+    document.querySelector('.tecla_pom'),
+    document.querySelector('.tecla_clap'),
+    document.querySelector('.tecla_tim'),
+    document.querySelector('.tecla_puff'),
+    document.querySelector('.tecla_splash'),
+    document.querySelector('.tecla_toim'),
+    document.querySelector('.tecla_psh'),
+    document.querySelector('.tecla_tic'),
+    document.querySelector('.tecla_tom'),
+]
+
+var audios = [
+    document.querySelector('#som_tecla_pom'),
+    document.querySelector('#som_tecla_clap'),
+    document.querySelector('#som_tecla_tim'),
+    document.querySelector('#som_tecla_puff'),
+    document.querySelector('#som_tecla_splash'),
+    document.querySelector('#som_tecla_toim'),
+    document.querySelector('#som_tecla_psh'),
+    document.querySelector('#som_tecla_tic'),
+    document.querySelector('#som_tecla_tom'),
+]
+
+var tecla_pom = teclas[0];
 tecla_pom.addEventListener('click', function() {
-    document.querySelector('#som_tecla_pom').play();
+    audios[0].play();
+});
+var tecla_pom = teclas[1];
+tecla_pom.addEventListener('click', function() {
+    audios[1].play();
+});
+var tecla_pom = teclas[2];
+tecla_pom.addEventListener('click', function() {
+    audios[2].play();
+});
+var tecla_pom = teclas[3];
+tecla_pom.addEventListener('click', function() {
+    audios[3].play();
+});
+var tecla_pom = teclas[4];
+tecla_pom.addEventListener('click', function() {
+    audios[4].play();
+});
+var tecla_pom = teclas[5];
+tecla_pom.addEventListener('click', function() {
+    audios[5].play();
+});
+var tecla_pom = teclas[6];
+tecla_pom.addEventListener('click', function() {
+    audios[6].play();
+});
+var tecla_pom = teclas[7];
+tecla_pom.addEventListener('click', function() {
+    audios[7].play();
+});
+var tecla_pom = teclas[8];
+tecla_pom.addEventListener('click', function() {
+    audios[8].play();
 });
