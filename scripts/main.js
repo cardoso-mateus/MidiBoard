13f6d8a9cfcee47f1c35783abd3d855d6eadaabf
Mateus Cardoso <cardoso.mateusv@gmail.com>
2024-08-10 21:39:31 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,29 @@
-var teclas = ['.tecla_pom','.tecla_clap','.tecla_tim','.tecla_puff',
-    '.tecla_splash','.tecla_toim','.tecla_psh','.tecla_tic','.tecla_tom',]
+var teclas = [
+    '.tecla_pom',
+    '.tecla_clap',
+    '.tecla_tim',
+    '.tecla_puff',
+    '.tecla_splash',
+    '.tecla_toim',
+    '.tecla_psh',
+    '.tecla_tic',
+    '.tecla_tom',
+]
 
-var audios = ['#som_tecla_pom','#som_tecla_clap','#som_tecla_tim','#som_tecla_puff',
-    '#som_tecla_splash','#som_tecla_toim','#som_tecla_psh','#som_tecla_tic','#som_tecla_tom',]
+var audios = [
+    '#som_tecla_pom',
+    '#som_tecla_clap',
+    '#som_tecla_tim',
+    '#som_tecla_puff',
+    '#som_tecla_splash',
+    '#som_tecla_toim',
+    '#som_tecla_psh',
+    '#som_tecla_tic',
+    '#som_tecla_tom',
+]
 
 function criaTecla(i) {
-    document.querySelector(teclas[i])
-    .addEventListener('click', function() {
+    document.querySelector(teclas[i]).addEventListener('click', function() {
         document.querySelector(audios[i]).play();
     });
 }
