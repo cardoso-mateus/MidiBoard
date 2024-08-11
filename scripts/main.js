f1a6290020e1be425da4be8394766670608a25f2
Mateus Cardoso <cardoso.mateusv@gmail.com>
2024-08-10 21:51:23 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,13 @@
 var teclas = [
-    '.tecla_pom',
-    '.tecla_clap',
-    '.tecla_tim',
-    '.tecla_puff',
-    '.tecla_splash',
-    '.tecla_toim',
-    '.tecla_psh',
-    '.tecla_tic',
-    '.tecla_tom',
+    '.tecla_pom', '.tecla_clap', '.tecla_tim',
+    '.tecla_puff', '.tecla_splash', '.tecla_toim',
+    '.tecla_psh', '.tecla_tic', '.tecla_tom',
 ]
 
 var audios = [
-    '#som_tecla_pom',
-    '#som_tecla_clap',
-    '#som_tecla_tim',
-    '#som_tecla_puff',
-    '#som_tecla_splash',
-    '#som_tecla_toim',
-    '#som_tecla_psh',
-    '#som_tecla_tic',
-    '#som_tecla_tom',
+    '#som_tecla_pom', '#som_tecla_clap', '#som_tecla_tim',
+    '#som_tecla_puff', '#som_tecla_splash', '#som_tecla_toim',
+    '#som_tecla_psh', '#som_tecla_tic', '#som_tecla_tom',
 ]
 
 function seletor(texto) {return document.querySelector(texto);}
